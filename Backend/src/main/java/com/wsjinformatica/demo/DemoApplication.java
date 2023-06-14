e93c99236860cfff8c69a503b196a25e34e27ded
unknown <williamsartijose@hotmail.com>
2023-06-14 15:40:04 +0100

    Refatorando a aplicação

--- Backend/src/main/java/com/wsjinformatica/demo/DemoApplication.java
+++ Backend/src/main/java/com/wsjinformatica/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.wsjinformatica.demo;
+package com.devsuperior.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Backend/src/test/java/com/demo/dscatalog/DemoApplicationTests.java
+++ Backend/src/test/java/com/demo/dscatalog/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wsjinformatica.demo;
+package com.demo.dscatalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
