c376104c043159b9b6fffa9e0740c021ffef6853
unknown <williamsartijose@hotmail.com>
2023-06-15 13:05:19 +0100

    Checklist do OAuth2 com JWT no Spring Boot

---
+++ src/main/java/com/wsjsistema/wsjdscommerce/config/ResourceServerConfig.java
@@ -0,0 +1,50 @@
+package com.wsjsistema.wsjdscommerce.config;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${cors.origins}")
+    private String corsOrigins;
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
+
+    private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
+
+    private static final String[] ADMIN = {"/users/**"};
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        // H2
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .anyRequest().authenticated();
+    }
+}
