355e12e96f84a946db419439138253e1172dac40
unknown <williamsartijose@hotmail.com>
2023-06-15 12:13:53 +0100

    Armazenamento da senha codificada com BCrypt

--- src/main/java/com/wsjsistema/wsjdscommerce/WsjdscommerceApplication.java
+++ src/main/java/com/wsjsistema/wsjdscommerce/WsjdscommerceApplication.java
@@ -1,13 +1,27 @@
 package com.wsjsistema.wsjdscommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication
-public class WsjdscommerceApplication {
+public class WsjdscommerceApplication implements CommandLineRunner {
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsjdscommerceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		String test = passwordEncoder.encode("123456");
+		System.out.println(test);
+
+	}
 }
